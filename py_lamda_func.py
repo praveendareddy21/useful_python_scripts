23796e7b070d1da746fe286d57ed5f3cb6296742
praveendareddy21 <praveendareddy@gmail.com>
2017-05-26 21:14:57 -0700

    added lamda func utility

---
+++ py_lamda_func.py
@@ -0,0 +1,10 @@
+"""
+From lambda calculus, (mostly used in function programming) refers to anonymous functions in programming.
+lamda along with closures provides powerful programming paradigm
+
+
+
+
+"""
+summer_func = lambda x, y : x + y
+print(summer_func(2, 3))
